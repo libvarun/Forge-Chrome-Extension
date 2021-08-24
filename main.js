daf61ace4e3b0029dc4103de68d08e06f242b80f
varun patil <varun.patil578@gmail.com>
2021-08-24 12:01:44 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('client_id').value = 'ZnOIAN39gAIYk5kwm1XtH54wc11WfbtH';
+document.getElementById('client_secret').value = 'F1ogSb38fApySAzu';
+document.getElementById("authenticate").click();
+setTimeout(() => {
+    document.querySelector(".jstree-ocl").click();    
+}, 1000);
